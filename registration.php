77d201fd064be3f80dc0556347e9c33aecd51335
todoroal <alexander.todorovic@stud.fh-campuswien.ac.at>
2021-05-22 20:25:54 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];
+    $gender = $_POST['gender'];
+    $emailaddress = $_POST['emailaddress'];
+    $phonenumber = $_POST['phonenumber'];
+    $address = $_POST['address'];
+    $postalcode = $_POST['postalcode'];
+    $checkbox = $_POST['checkbox'];
+?>
