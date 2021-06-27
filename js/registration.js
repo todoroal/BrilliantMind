a67ee44621a89664946b748df2bcfa1cb5d3ddf4
nadinefeurhuber <nadine.feurhuber@outlook.de>
2021-06-27 16:14:05 +0200

    Registration Post

--- js/registration.js
+++ js/registration.js
@@ -4,15 +4,20 @@ box.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const formData = new FormData(this);
+    const searchParams = new URLSearchParams();
+
+    for (const pair of formData) {
+        searchParams.append(pair[0], pair[1]);
+    }
 
     fetch ('registration.html', {
         method: 'post',
-        body: formData
+        body: searchParams
         }).then(function (response) {
             return response.text();
         }).then(function (text) {
-
-    }
-
-
-}
+            console.log(text);
+        }).catch(function (error) {
+        console.error(error)
+    })
+});
